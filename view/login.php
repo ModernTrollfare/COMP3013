ce24466f18a9f60a2ae0b75e7a237eaf3896eabf
ModernTrollfare <a123456613@gmail.com>
2015-02-06 18:48:42 +0000

    Updates: 	view/index.php - changed to .php file to cater for invalid login 	messages
    	view/login.html - just useless. better remove it

--- view/login.php
+++ view/login.php
@@ -1,8 +1,26 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<body>
 <?php
-echo "Hello";
+	//$connection = mysqli_connect('localhost','foo','bar','Example') or die('Error connecting to MySQL server.'. mysql_error());
+	$usertype = $_POST["UserType"]
+	$user = array();
+	$user["UserID"] = $_POST["UserID"];
+	$user["Password"] = $_POST["Password"];
+	if($usertype == "-1"){
+		$message = "Please Select your User Type.";
+		header('Location: login.php?message='.$message);
+		exit;
+	}
+/*	if($usertype == "0")
+		$query = "SELECT * FROM ADMINS WHERE admin_id =".$user["UserID"]." AND pwd = ".$user["Password"];
+	else
+		$query = "SELECT * FROM STUDENTS WHERE student_id =". $user["UserID"]. "AND pwd =". $user["Password"];
+	$result = mysqli_query($connection,$query) or die('Error making select users query' . mysql_error());
+	if (!$result){
+		$message = "Your Login Credentials Are Incorrect. Check it again!";
+		header('Location: login.php?message='.$message);
+		exit;
+	}
+	else{*/
+		session_start();
+		header('Location: logged_in.html'); 
+//	}
 ?>
-</body>
-</html>
