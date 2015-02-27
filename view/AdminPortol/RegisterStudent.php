89e803cb31fe76f55d9d8bc25f61977643c592f3
tins820 <tins820@live.hk>
2015-02-27 19:38:39 +0000

    removed exit()
    20150227 1938

--- view/AdminPortol/RegisterStudent.php
+++ view/AdminPortol/RegisterStudent.php
@@ -5,7 +5,6 @@
         if(!((isset($_SESSION['user']))&&(isset($_SESSION['password'])))){
             $_SESSION['errors'] = array("Please Login before proceeding.");
             header("Location: ../index.php");
-            exit();
         }
         if($_SESSION['Usertype'] != '0'){
             print("Well - You do not have the permission to access this page. You will be redirected to you home page in 5 seconds.");
