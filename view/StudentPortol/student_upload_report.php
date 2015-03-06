ab1a4bb620ea880042eea2feed21f5abce7ec1e4
tins820 <tins820@live.hk>
2015-03-06 18:12:08 +0000

    uploaded missing content
    20150306 1812

--- view/StudentPortol/student_upload_report.php
+++ view/StudentPortol/student_upload_report.php
@@ -7,15 +7,29 @@
 	$date = date('Y-m-d H:i:s');
 
 	$query = "SELECT group_id FROM groups WHERE sid_1 = '$userid' OR sid_2 = '$userid' OR sid_3 = '$userid'";
+	$result = mysqli_query($connection, $query) or die('Error' . mysqli_error($connection));
 
-	$realuid = mysqli_fetch_row(mysqli_query($query));
+	if (mysqli_num_rows($result) != 1){
+		$_SESSION['errors'] = array("cannot find group_id");
+		header('Location: upload_report_2.php');
+		exit;
+	}
+
+	$realuid = mysqli_fetch_row($result);
 	$groupid = $realuid[0];
 
 	$query = "INSERT INTO reports (text_file, group_id, last_modified) VALUES ($content, $groupid, $date)";
 
-	echo '<script language="javascript">
-alert("upload successful! It will direct you back to Main Page in 5 secs")
-</script>';
+	if (mysqli_query($query) != true) {
+		echo '<script language="javascript">'
+		echo'alert("upload unsuccessful! It will direct you backe in 5 secs")'
+		echo '</script>'
+		header('refresh:5; url=upload_report_2.php')
+	}
+
+	echo '<script language="javascript">'
+	echo 'alert("upload successful! It will direct you back to Main Page in 5 secs")'
+	echo '</script>';
 
 	header('refresh:5; url=student_main.php');
 
