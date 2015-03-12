6c2bda8b56956406256b95f515251a3cf6773994
Mengdan <zhaomengdan93@gmail.com>
2015-03-12 14:24:05 +0000

    reversed my changes

--- view/StudentPortol/view_each_allocated_assignments.php
+++ view/StudentPortol/view_each_allocated_assignments.php
@@ -102,7 +102,7 @@
               <!-- <li><a href="#">Link</a></li>
               <li><a href="#">Link</a></li> -->
               <li class="nav-header">Assessments</li>
-              <li class="active"><a href="view_allocated_assignments.php">Viewing Allocated Assignments</a></li>
+              <li class="active"><a href="view_allocated_assignments.php">Assessing other groups</a></li>
               <li><a href="view_grades_and_comments.php">Viewing Grades and Comments from Others</a></li>
               <!-- <li><a href="#">Link</a></li>
               <li><a href="#">Link</a></li>
