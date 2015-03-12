a2c2c73f2793ed5b3d783443e3a8bc009e48ddb1
ModernTrollfare <a123456613@gmail.com>
2015-03-12 16:44:08 +0000

    new file:   view/StudentPortol/getReport.php

---
+++ view/StudentPortol/getReport.php
@@ -0,0 +1,172 @@
+<?php session_start();
+    if(!((isset($_SESSION['username']))&&(isset($_SESSION['password'])))){
+    header("Location: ../index.php");
+    $_SESSION['errors'] = array("Please Login before proceeding.");
+  }
+  else if($_SESSION['usertype'] != '1'){
+    header("Refresh:3;url=../index.php");
+    print("Well - You do not have the permission to access this page. You will be redirected to your home page in 3 seconds.");
+    exit;
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>View Assignments</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="../../lib/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+      @media (max-width: 980px) {
+        /* Enable use of floated navbar text */
+        .navbar-text.pull-right {
+          float: none;
+          padding-left: 5px;
+          padding-right: 5px;
+        }
+      }
+    </style>
+    <link href="../../lib/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">Peer Assessment System</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="student_main.php">Home</a></li>
+              <li><a href="about.php">About</a></li>
+              <!-- <li><a href="#contact">Contact</a></li> -->
+              <!-- <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider"></li>
+                  <li class="nav-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li> -->
+            </ul>
+            <form class="navbar-form pull-right" action="../logout.php">
+              <span class="input-group-addon" id="user-greeting" style="color:white">Hi <?php print($_SESSION['username']);?></span>
+              <button type="submit" class="btn">Sign Out</button>
+            </form>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row-fluid">
+        <div class="span3">
+          <div class="well sidebar-nav">
+            <ul class="nav nav-list">
+              <li><a href="student_main.php">Main Page</a></li>
+              <li class="nav-header">Assignments</li>
+              <li><a href="upload_assignment.php">Uploading Assignments</a></li>
+              <li><a href="student_rank.php">Ranking</a></li>
+              <!-- <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li> -->
+              <li class="nav-header">Assessments</li>
+              <li class="active"><a href="view_allocated_assignments.php">Assessing other groups</a></li>
+              <li><a href="view_grades_and_comments.php">Viewing Grades and Comments from Others</a></li>
+              <!-- <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li>
+              <li><a href="#">Link</a></li> -->
+              <li class="nav-header">Personal and Group Details</li>
+              <li><a href="change_group_details.php">Changing Group Details</a></li>
+              <li><a href="change_password.php">Changing Personal Password</a></li>
+              <!-- <li><a href="#">Link</a></li> -->
+            </ul>
+          </div><!--/.well -->
+        </div><!--/span-->
+        <?php
+            $connection = mysqli_connect('localhost','toor','toor','comp3013') or die('Error connecting to MySQL server.'. mysqli_error($connection));
+            $stuid = $_SESSION['userid'];
+            $query = "SELECT * FROM GROUPS WHERE student_1 = '$stuid' OR student_2 = '$stuid' OR student_3 = '$stuid'";
+            $result = mysqli_query($connection, $query)
+              or die('Error Query'.mysqli_error($connection));
+        ?>
+        <div class="span9">
+          <div class="hero-unit">
+           <?php
+           if(!isset($_POST['fetchid'])){
+           	echo 'You are not supposed to be here. Please go back to the previous page.';
+           	exit;
+           }
+	$fetch = $_POST['fetchid'];
+	$connection = mysqli_connect('localhost','toor','toor','comp3013') or die('Error connecting to MySQL server.'. mysqli_error($connection));
+	$query = "SELECT xml_file from REPORTS where group_id = '$fetch'";
+	$result = mysqli_query($connection,$query);
+	$temp = mysqli_fetch_assoc($result);
+	$filepath = $temp['xml_file'];
+	echo '<button class="btn" href="../'.$filepath.'">Click to get file</button><br></br>';
+	echo '<button class="btn" href="view_allocated_assignments.php">Back to Assessments</button>';
+?>
+        </div><!--/span-->
+      </div><!--/row-->
+
+      <hr>
+
+      <footer>
+        <p>&copy; Peer Assessment System</p>
+      </footer>
+
+    </div><!--/.fluid-container-->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap-transition.js"></script>
+    <script src="../assets/js/bootstrap-alert.js"></script>
+    <script src="../assets/js/bootstrap-modal.js"></script>
+    <script src="../assets/js/bootstrap-dropdown.js"></script>
+    <script src="../assets/js/bootstrap-scrollspy.js"></script>
+    <script src="../assets/js/bootstrap-tab.js"></script>
+    <script src="../assets/js/bootstrap-tooltip.js"></script>
+    <script src="../assets/js/bootstrap-popover.js"></script>
+    <script src="../assets/js/bootstrap-button.js"></script>
+    <script src="../assets/js/bootstrap-collapse.js"></script>
+    <script src="../assets/js/bootstrap-carousel.js"></script>
+    <script src="../assets/js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>
+Status API Training Shop Blog About
+© 2015 GitHub, Inc. Terms Privacy Security Contact
