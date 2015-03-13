e93c317089bd717b0831d790b0b7e2c95ceeed34
tins820 <tins820@live.hk>
2015-03-13 18:40:20 +0000

    added upload xml file
    20150313 1840

--- view/upload.php
+++ view/upload.php
@@ -4,6 +4,14 @@
     $target_file = $target_dir .sha1($_SESSION['userid']). basename($_FILES["fileToUpload"]["name"]);
     $fail = 1;
     $doctype = pathinfo($target_file,PATHINFO_EXTENSION);
+
+    $docmode = 2; // 0 for xml; 1 for txt // added by tins
+
+    if ($doctype == "xml")  // added by tins
+        $docmode = 0; // added by tins
+    if ($doctype == "txt") // added by tins
+        $docmode = 1; // added by tins
+
     // var_dump($doctype);
     // var_dump($target_file);
     // var_dump($_FILES);
@@ -30,19 +38,30 @@
                 $gid = $myrow['group_id'];
                 $query = "SELECT * FROM REPORTS WHERE group_id = '$gid'";
                 $result = mysqli_query($connection, $query);
+
+                if ($docmode == 0) { // added by tins
+                    $xml = simplexml_load_file($target_file); // added by tins
+
+                    $title = $xml->title; // added by tins
+                    $content = $xml->content; // added by tins
+                } // added by tins
+
                 if(mysqli_num_rows($result)== 0){
                     $query = "SELECT MAX(report_id) AS maxid FROM REPORTS";
                     $result = mysqli_fetch_assoc(mysqli_query($connection, $query));
                     if (is_null($result['maxid']))
                         $nrid = 0;
                     else $nrid = $result['maxid']+1;
-                    $query = "INSERT INTO REPORTS (report_id,group_id,xml_file,last_modified) VALUES ('$nrid','$gid','$target_file','$date');";
+                    if ($docmode == 1) // added by tins
+                        $query = "INSERT INTO REPORTS (report_id,group_id,text,last_modified) VALUES ('$nrid','$gid','$target_file','$date');";
+                    if ($docmode == 0) // added by tins
+                        $query = "INSERT INTO REPORTS (report_id, group_id, xml_file, last_modified, xml_title, xml_content) VALUES ('$maxid', '$gid', '$target_file', '$date', '$title', '$content');"; // added by tins
                 }
                 else{
-                    $query = "UPDATE REPORTS SET 
-                    xml_file = '$target_file',
-                    last_modified = '$date'
-                    WHERE group_id = '$gid'";
+                    if ($docmode == 1) // added by tins
+                        $query = "UPDATE REPORTS SET text = '$target_file', last_modified = '$date' WHERE group_id = '$gid'";
+                    if ($docmode == 0) // added by tins
+                        $query = "UPDATE REPORTS SET xml_file = '$target_file' , last_modified = '$date' , xml_title = '$title' , xml_content = '$content', text = NULL WHERE group_id = '$gid'"; // added by tins
                 }
                 $result = mysqli_query($connection, $query) or die('Error' . mysqli_error($connection));
             $_SESSION['uperror'] = "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
