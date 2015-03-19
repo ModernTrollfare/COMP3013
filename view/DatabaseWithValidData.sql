9383b64491ca93cf44e4bae022fc883c26c556fa
Mengdan <zhaomengdan93@gmail.com>
2015-03-19 14:07:00 +0000

    deleted redundant sql files, added a sql with valid data

--- view/DatabaseWithValidData.sql
+++ view/DatabaseWithValidData.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.6
+-- version 4.2.10
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 13, 2015 at 06:23 PM
--- Server version: 5.6.16
--- PHP Version: 5.5.11
+-- Host: localhost:8889
+-- Generation Time: Mar 19, 2015 at 03:04 PM
+-- Server version: 5.5.38
+-- PHP Version: 5.6.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `comp3013`
@@ -26,7 +20,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `admins`
 --
 
-CREATE TABLE IF NOT EXISTS `admins` (
+CREATE TABLE `admins` (
   `admin_id` int(9) NOT NULL,
   `name` varchar(30) COLLATE ascii_bin NOT NULL,
   `pwd` varchar(50) COLLATE ascii_bin NOT NULL
@@ -49,7 +43,7 @@ INSERT INTO `admins` (`admin_id`, `name`, `pwd`) VALUES
 -- Table structure for table `assessments`
 --
 
-CREATE TABLE IF NOT EXISTS `assessments` (
+CREATE TABLE `assessments` (
   `assessment_id` int(9) NOT NULL,
   `report_id` int(9) NOT NULL,
   `group_id` int(9) NOT NULL,
@@ -71,10 +65,26 @@ INSERT INTO `assessments` (`assessment_id`, `report_id`, `group_id`, `grade`, `c
 -- Table structure for table `assignations`
 --
 
-CREATE TABLE IF NOT EXISTS `assignations` (
+CREATE TABLE `assignations` (
   `group_to_be_assessed` int(11) NOT NULL,
   `group_assessing` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `assignations`
+--
+
+INSERT INTO `assignations` (`group_to_be_assessed`, `group_assessing`) VALUES
+(1, 0),
+(4, 0),
+(0, 1),
+(2, 1),
+(1, 2),
+(3, 2),
+(2, 3),
+(4, 3),
+(0, 4),
+(3, 4);
 
 -- --------------------------------------------------------
 
@@ -82,15 +92,15 @@ CREATE TABLE IF NOT EXISTS `assignations` (
 -- Table structure for table `forum`
 --
 
-CREATE TABLE IF NOT EXISTS `forum` (
+CREATE TABLE `forum` (
 `thread_id` int(6) unsigned NOT NULL,
   `parentThread` int(6) unsigned NOT NULL DEFAULT '0',
   `title` varchar(100) DEFAULT NULL,
   `message` text,
-  `posttime` date NOT NULL DEFAULT '0000-00-00',
-  `group_id` int(9) NOT NULL,
+  `posttime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `lastreplytime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `student_id` int(9) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -98,7 +108,7 @@ CREATE TABLE IF NOT EXISTS `forum` (
 -- Table structure for table `groups`
 --
 
-CREATE TABLE IF NOT EXISTS `groups` (
+CREATE TABLE `groups` (
   `group_id` int(9) NOT NULL,
   `student_1` int(9) DEFAULT NULL,
   `student_2` int(9) DEFAULT NULL,
@@ -112,7 +122,9 @@ CREATE TABLE IF NOT EXISTS `groups` (
 INSERT INTO `groups` (`group_id`, `student_1`, `student_2`, `student_3`) VALUES
 (0, 0, 1, 2),
 (1, 3, 4, 5),
-(2, 6, NULL, NULL);
+(2, 6, 7, 8),
+(3, 9, 10, 11),
+(4, 12, 13, NULL);
 
 -- --------------------------------------------------------
 
@@ -120,11 +132,10 @@ INSERT INTO `groups` (`group_id`, `student_1`, `student_2`, `student_3`) VALUES
 -- Table structure for table `reports`
 --
 
-CREATE TABLE IF NOT EXISTS `reports` (
+CREATE TABLE `reports` (
   `report_id` int(9) NOT NULL,
   `text` text COLLATE ascii_bin NOT NULL,
   `xml_file` longblob,
-  `over_grade` double DEFAULT NULL,
   `group_id` int(9) NOT NULL,
   `last_modified` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin;
@@ -133,10 +144,10 @@ CREATE TABLE IF NOT EXISTS `reports` (
 -- Dumping data for table `reports`
 --
 
-INSERT INTO `reports` (`report_id`, `text`, `xml_file`, `over_grade`, `group_id`, `last_modified`) VALUES
-(0, 'this is a test report for group 1', 0x75706c6f6164732f333536613139326237393133623034633534353734643138633238643436653633393534323861626d6574612e786d6c, NULL, 0, '2015-03-13'),
-(1, 'this is a test report for group 2', 0x75706c6f6164732f616333343738643639613363383166613632653630663563333639363136356134653565366163346d6574612e786d6c, NULL, 1, '2015-03-13'),
-(2, '', 0x75706c6f6164732f633164666439366565613863633262363237383532373562636133386163323631323536653237386d6574612e786d6c, NULL, 2, '2015-03-13');
+INSERT INTO `reports` (`report_id`, `text`, `xml_file`, `group_id`, `last_modified`) VALUES
+(0, 'this is a test report for group 0', 0x75706c6f6164732f333536613139326237393133623034633534353734643138633238643436653633393534323861626d6574612e786d6c, 0, '2015-03-13'),
+(1, 'this is a test report for group 1', 0x75706c6f6164732f616333343738643639613363383166613632653630663563333639363136356134653565366163346d6574612e786d6c, 1, '2015-03-13'),
+(2, 'this is a test report for group 1', 0x75706c6f6164732f633164666439366565613863633262363237383532373562636133386163323631323536653237386d6574612e786d6c, 2, '2015-03-13');
 
 -- --------------------------------------------------------
 
@@ -144,7 +155,7 @@ INSERT INTO `reports` (`report_id`, `text`, `xml_file`, `over_grade`, `group_id`
 -- Table structure for table `students`
 --
 
-CREATE TABLE IF NOT EXISTS `students` (
+CREATE TABLE `students` (
   `student_id` int(9) NOT NULL,
   `name` varchar(30) COLLATE ascii_bin NOT NULL,
   `pwd` varchar(40) COLLATE ascii_bin NOT NULL
@@ -155,13 +166,21 @@ CREATE TABLE IF NOT EXISTS `students` (
 --
 
 INSERT INTO `students` (`student_id`, `name`, `pwd`) VALUES
-(0, 'student1', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
-(1, 'student2', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
-(2, 'student3', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
-(3, 'student4', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
-(4, 'student5', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
-(5, 'student6', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
-(6, 'Test9', '61c239e5c7203949a78ff72615256c8db5dc04c4');
+(0, 'Lorriane Spears', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(1, 'Sheena Danks', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(2, 'Megan Stutz', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(3, 'Flavia Logsdon', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(4, 'Jenniffer Giffin', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(5, 'Ethelyn Peterman', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(6, 'Gilberto Calvery', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(7, 'Omega Heart', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(8, 'Susanna Ruehl', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(9, 'Judith Delnero', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(10, 'Francoise Strebel', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(11, 'Carman Harton', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(12, 'Kermit Gresham', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(13, 'Kieth Ewing', '61c239e5c7203949a78ff72615256c8db5dc04c4'),
+(14, 'Kaye Toms', '61c239e5c7203949a78ff72615256c8db5dc04c4');
 
 --
 -- Indexes for dumped tables
@@ -217,7 +236,7 @@ ALTER TABLE `students`
 -- AUTO_INCREMENT for table `forum`
 --
 ALTER TABLE `forum`
-MODIFY `thread_id` int(6) unsigned NOT NULL AUTO_INCREMENT;
+MODIFY `thread_id` int(6) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
 --
 -- Constraints for dumped tables
 --
@@ -233,8 +252,8 @@ ADD CONSTRAINT `assessments_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `reports
 -- Constraints for table `assignations`
 --
 ALTER TABLE `assignations`
-ADD CONSTRAINT `assignations_ibfk_2` FOREIGN KEY (`group_assessing`) REFERENCES `groups` (`group_id`),
-ADD CONSTRAINT `assignations_ibfk_1` FOREIGN KEY (`group_to_be_assessed`) REFERENCES `groups` (`group_id`);
+ADD CONSTRAINT `assignations_ibfk_1` FOREIGN KEY (`group_to_be_assessed`) REFERENCES `groups` (`group_id`),
+ADD CONSTRAINT `assignations_ibfk_2` FOREIGN KEY (`group_assessing`) REFERENCES `groups` (`group_id`);
 
 --
 -- Constraints for table `groups`
@@ -249,7 +268,3 @@ ADD CONSTRAINT `groups_ibfk_3` FOREIGN KEY (`student_3`) REFERENCES `students` (
 --
 ALTER TABLE `reports`
 ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
